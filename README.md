054bd271f7cbc0eca709a27bfa6a97e802e176e4
Khandakar227 <xenonknight54@gmail.com>
2023-05-03 03:31:25 +0600

    A readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# ESP-Firmata
+A simple script allowing you to use **StandardFirmataWifi** library with ESP8266.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // @ts-check
-const {Board, Led} = require('johnny-five');
-const {EtherPortClient} = require('etherport-client');
-const {resolveAny} = require("dns");
+const { Board, Led } = require('johnny-five');
+const { EtherPortClient } = require('etherport-client');
 
 const port = new EtherPortClient({
     host: '192.168.0.110',
@@ -15,7 +14,7 @@ const board = new Board({
 board.on('ready', () => {
     console.log('Ready!!!');
     //Tell the board to do whatever you want
-        const led = new Led(2);
-        led.blink(1000);
+    const led = new Led(2);
+    led.blink(1000);
     //
 })
