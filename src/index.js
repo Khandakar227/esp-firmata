6f7500b0c7a12765d9406060a2a8cb3b3bbc7249
Khandakar227 <xenonknight54@gmail.com>
2023-05-03 03:07:13 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// @ts-check
+const {Board, Led} = require('johnny-five');
+const {EtherPortClient} = require('etherport-client');
+const {resolveAny} = require("dns");
+
+const port = new EtherPortClient({
+    host: '192.168.0.110',
+    port: 3030
+})
+
+const board = new Board({
+    port,
+})
+
+board.on('ready', () => {
+    console.log('Ready!!!');
+    //Tell the board to do whatever you want
+        const led = new Led(2);
+        led.blink(1000);
+    //
+})
